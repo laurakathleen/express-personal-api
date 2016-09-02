08ebfaf2e4ba54e25a71f21e58abf460b8ec9a73
laurakathleen <laurakwalley@gmail.com>
2016-09-02 12:58:57 -0700

    adjusts pet to new schema, edits seed.js

--- models/profile.js
+++ models/profile.js
@@ -1,19 +1,21 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
+var PetSchema = new Schema({
+	type: String,
+	name: String,
+	breed: String,
+	age: Number,
+	image: String
+})
+
 var ProfileSchema = new Schema({
    name: String,
    githubLink: String,
    githubProfileImage: String,
    personalSiteLink: String,
    currentCity: String,
-   pets: {
-		type: String,
-		name: String,
-		breed: String,
-		age: Number,
-		image: String
-	}
+   pets: [PetSchema]
 });
 
 var Profile = mongoose.model('Profile', ProfileSchema);
--- seed.js
+++ seed.js
@@ -3,42 +3,39 @@
 
 var db = require('./models');
 
-var Laura = new Profile({
+var new_profile = ({
 	name: "Laura Russell",
 	githubLink: "https://github.com/laurakathleen",
 	githubProfileImage: String,
 	personalSiteLink: "https://laurakathleen.github.io/#home-tab",
 	currentCity: "Burlingame",
-	pets: {
+	pets: [{
 		type: "dog",
 		name: "Meechum",
 		breed: "Australian Shepherd",
 		age: 1.5,
 		image: "public/images/Meechum.JPG"
-	}
+	}]
 });
 
-db.Profile.create(new_profile, function(err, profile){
-	if (err){
-		return console.log("Error:", err);
-	}
-
-	console.log("Created new profile", profile._id)
-	process.exit(); // we're all done! Exit the program.
-	})
-
-Laura.save(function(err, newProfile){
-	if(err) {return console.log(err);}
-	console.log("saved new profile: ", newProfile);
+db.Profile.remove({}, function (err, profiles) {
+  console.log('removed all profiles');
+  db.Profile.create(new_profile, function (err, profile){
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Created new profile", profile._id);
+    }
+});
 });
 
-var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
-db.Campsite.create(new_campsite, function(err, campsite){
-	if (err){
-		return console.log("Error:", err);
-	}
+// db.Campsite.create(new_campsite, function(err, campsite){
+// 	if (err){
+// 		return console.log("Error:", err);
+// 	}
 
-	console.log("Created new campsite", campsite._id)
-	process.exit(); // we're all done! Exit the program.
-	})
+// 	console.log("Created new campsite", campsite._id)
+// 	process.exit(); // we're all done! Exit the program.
+// 	})
