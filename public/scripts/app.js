68444d15c068d0395b0ec3912b9db44852280256
laurakathleen <laurakwalley@gmail.com>
2016-09-01 21:15:38 -0700

    works on ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,23 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	//array to hold all place data from API
+	var allPlaces = [];
+	console.log("ajax is all good");
+	$.ajax({
+		method: "GET",
+		url: '/api/profile',
+		success: onSuccess,
+		error: onError
+	})
 
-// your code
+	function onSuccess(json){
+		console.log(json);
+		allPlaces = json;
+		render();
+	}
 
+	function onError(json){
+		console.log('mistake');
+	}
 });
